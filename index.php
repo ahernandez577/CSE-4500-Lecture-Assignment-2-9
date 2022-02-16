5b4d12adfded36f4ed5df1a5cf1469fe33a1cb8d
Alex Hernandez <55168529+ahernandez577@users.noreply.github.com>
2022-02-15 21:40:36 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    $json_file = file_get_contents('my_data.json');
+    $json_data = json_decode($json_file,true);
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -13,54 +18,53 @@
     <div class="left-nav">
       <nav id="navbar-example3" class="navbar vertical-center flex-column center">
         <nav class="nav flex-column">
+          <img src="avatar.png" style="width:160px" alt="">
           <a class="nav-link" href="#about">About</a>
           <a class="nav-link" href="#education">Education</a>
           <a class="nav-link" href="#experience">Experience</a>
-          <a class="nav-link" href="#skills">Skills</a>
-          <a class="nav-link" href="#interests">Interests</a>
         </nav>
       </nav>
     </div>
+
     <div data-bs-offset="0" tabindex="0">
       <div id="about" class="content-item">
         <div class="vertical-center">
-          <h1>Alex Hernandez</h1>
-          <div class="subheading">
-            5500 University Pkwy, San Bernardino, CA 92407 · (123)456-7890
+          <h1><?php echo $json_data['first-name']; ?><span class="text-primary"><?php echo $json_data['last-name']; ?></span></h1>
+          <div class="subheading mb-3">
+            <?php echo $json_data['address']; ?> · <?php echo $json_data['phone']; ?> ·
+            <a href="<?php echo $json_data['email']; ?>"><?php echo $json_data['email']; ?></a>
           </div>
-          <p>...</p>
+          <p><?php echo $json_data['introduction']; ?></p>
         </div>
       </div>
       <hr>
       <div id="education" class="content-item">
         <div class="vertical-center">
-          <h4>Education</h4>
+          <h2>Education</h2>
           <p>...</p>
         </div>
       </div>
       <hr>
       <div id="experience" class="content-item">
         <div class="vertical-center">
-          <h4>Experience</h4>
-          <p>...</p>
-        </div>
-      </div>
-      <hr>
-      <div id="skills" class="content-item">
-        <div class="vertical-center">
-          <h4>Skills</h4>
-          <p>...</p>
-        </div>
-      </div>
-      <hr>
-      <div id="interests" class="content-item">
-        <div class="vertical-center">
-          <h4>Interests</h4>
-          <p>...</p>
+          <h2>Experience</h2>
+          <?php foreach($json_data['experience'] AS $experience) { ?>
+              <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                <div class="flex-grow-1">
+                  <h3 class="mb-0"><?php echo $experience['title']; ?></h3>
+                  <div class="subheading mb-3"><?php echo $experience['employer']; ?></div>
+                  <p><?php echo $experience['description']; ?></p>
+                </div>
+                <div class="flex-shrink-0">
+                  <span class="text-primary"><?php echo $experience['period']; ?></span>
+                </div>
+              </div>
+          <?php  } ?>
         </div>
       </div>
       <hr>
     </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
